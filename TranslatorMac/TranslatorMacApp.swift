d5695cfc960c5c88af361068de9b7ce7392cbb7d
Iosi Pratama <pratamaiosi@gmail.com>
2025-12-19 20:15:40 +0700

    Initial Commit

---
+++ TranslatorMac/TranslatorMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorMacApp.swift
+//  TranslatorMac
+//
+//  Created by Iosi Pratama on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
